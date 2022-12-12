93ce4e452730ff1e8eeeb48f3cebf38cde79e3c0
Martin Hicks <hello@martinhicks.net>
2022-12-12 20:38:28 +0000

    sessions are just objects

--- server/csrf.js
+++ server/csrf.js
@@ -1,26 +1,26 @@
 import { v4 as uuid } from "uuid";
 
 export function createCsrfToken(req) {
-    let csrf_token = req.session.get('csrf');
+    let csrf_token = req.session['csrf']
 
     if (typeof csrf_token === "string") {
         return csrf_token;
     } else {
         let newCsrfToken = uuid();
-        req.session.set('csrf', newCsrfToken);
+        req.session['csrf'] = newCsrfToken;
     }
     
 }
 
 export function verifyCsrfToken(req) {
     
-    if (!req.body.get('csrf')) {
+    if (!req.body['csrf']) {
         throw Error({
             message: "Could not find CSRF token in request body",
         });
     }
 
-    if (req.body.get('csrf') !== req.session.get('csrf')) {
+    if (req.body['csrf'] !== req.session['csrf']) {
         throw Error({
             message: "Could not verify CSRF token.",
         });
