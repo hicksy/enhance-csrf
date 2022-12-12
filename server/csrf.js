d226799a338112597895583711e4d215b9f93f90
Martin Hicks <hello@martinhicks.net>
2022-12-12 20:26:22 +0000

    think rely on the middleware mechanism would be best

--- server/csrf.js
+++ server/csrf.js
@@ -1,31 +1,29 @@
 import { v4 as uuid } from "uuid";
 
-export default function createCsrfToken(session) {
-    let csrf_token = session.get('csrf');
+export default function createCsrfToken() {
+    let csrf_token = req.session.get('csrf');
 
     if (typeof csrf_token === "string") {
         return csrf_token;
     } else {
         let newCsrfToken = uuid();
-        session.set('csrf', newCsrfToken);
-        return newCsrfToken;
+        req.session.set('csrf', newCsrfToken);
     }
     
 }
 
 export default function verifyCsrfToken() {
     
-    if (req.body.get('csrf') !== req.session.get('csrf')) {
-        throw Error({
-            message: "Could not verify CSRF token.",
-        });
-    }
-
     if (!req.body.get('csrf')) {
         throw Error({
             message: "Could not find CSRF token in request body",
         });
     }
 
+    if (req.body.get('csrf') !== req.session.get('csrf')) {
+        throw Error({
+            message: "Could not verify CSRF token.",
+        });
+    }
     
 }
