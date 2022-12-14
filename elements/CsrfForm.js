8428ca181a4b6ea44474549d37880b64ab0fa01b
Martin Hicks <hello@martinhicks.net>
2022-12-14 11:06:47 +0000

    as a function?

--- elements/CsrfForm.js
+++ elements/CsrfForm.js
@@ -4,11 +4,9 @@ export default function CsrfForm({ html, state }) {
     const { attrs={} } = state
     const { action = '', method = '' } = attrs
 
-    customElements.define("csrf-input", CsrfInput);
-
     return html`
     <form action="${action}" method="${method}">
-        <csrf-input></csrf-input>
+        ${CsrfInput()}
         <slot></slot>
     </form>
 `
