b366bbe707b7a2686a971911e89df1d8dd4b7979
Martin Hicks <hello@martinhicks.net>
2023-05-15 16:03:52 +0100

    support for verifying csrf token within a multipart/form-data encoded body

--- server/csrf.js
+++ server/csrf.js
@@ -1,5 +1,6 @@
 import { v4 as uuid } from "uuid";
 import arc from '@architect/functions';
+import multipart from 'lambda-multipart-parser'
 
 export async function createCsrfToken(req) {
     let session = await arc.http.session.read(req)
@@ -11,13 +12,22 @@ export async function createCsrfToken(req) {
     req.session.csrf = session.csrf;
 }
 
-export function verifyCsrfToken(req) {
+export async function verifyCsrfToken(req) {
+   
+    let providedCsrf;
     
-    if (!req.body['csrf']) {
+    if(req.headers['content-type'].includes('multipart/form-data')) {
+        const multiPartBody = await multipart.parse({...req, body: req.body.base64 ? req.body.base64 : req.rawBody});
+        providedCsrf = multiPartBody.csrf;
+    } else {
+        providedCsrf = req.body.csrf;
+    }
+    
+    if (!providedCsrf) {
         throw Error("Could not find CSRF token in request body");
     }
 
-    if (req.body['csrf'] !== req.session['csrf']) {
+    if (providedCsrf !== req.session['csrf']) {
         throw Error("Could not verify CSRF token.");
     }
     
