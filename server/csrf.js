8f195b13c8d8ba5684667db4ea1c3c1fa08fbdcd
Martin Hicks <hello@martinhicks.net>
2025-02-21 16:40:33 +0000

    v1.0.0 - session.read to support arcs database sessions. Pass class attribute from web component to inner form element.

--- server/csrf.js
+++ server/csrf.js
@@ -1,6 +1,55 @@
 import { v4 as uuid } from "uuid";
 import arc from '@architect/functions';
-import multipart from 'lambda-multipart-parser'
+import busboy from 'busboy';
+
+
+/* Standalone parse function to use new busboy version. Modified from original code `https://github.com/francismeynard/lambda-multipart-parser.git` */
+
+const parse = (event) => new Promise((resolve, reject) => {
+    const _busboy = busboy({
+        headers: {
+            'content-type': event.headers['content-type'] || event.headers['Content-Type']
+        }
+    });
+    const result = {
+        files: []
+    };
+
+    _busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        const uploadFile = {};
+
+        file.on('data', data => {
+            uploadFile.content = data;
+        });
+
+        file.on('end', () => {
+            if (uploadFile.content) {
+                uploadFile.filename = filename;
+                uploadFile.contentType = mimetype;
+                uploadFile.encoding = encoding;
+                uploadFile.fieldname = fieldname;
+                result.files.push(uploadFile);
+            }
+        });
+    });
+
+    _busboy.on('field', (fieldname, value) => {
+        result[fieldname] = value;
+    });
+
+    _busboy.on('error', error => {
+        reject(error);
+    });
+
+    _busboy.on('finish', () => {
+        resolve(result);
+    });
+
+    const encoding = event.encoding || (event.isBase64Encoded ? "base64" : "binary");
+
+    _busboy.write(event.body, encoding);
+    _busboy.end();
+});
 
 export async function createCsrfToken(req) {
     let session = await arc.http.session.read(req)
@@ -15,9 +64,10 @@ export async function createCsrfToken(req) {
 export async function verifyCsrfToken(req) {
    
     let providedCsrf;
+    let session = await arc.http.session.read(req)
     
     if(req.headers['content-type'].includes('multipart/form-data')) {
-        const multiPartBody = await multipart.parse({...req, body: req.body.base64 ? req.body.base64 : req.rawBody});
+        const multiPartBody = await parse({...req, body: req.body.base64 ? req.body.base64 : req.rawBody});
         providedCsrf = multiPartBody.csrf;
     } else {
         providedCsrf = req.body.csrf;
@@ -27,7 +77,7 @@ export async function verifyCsrfToken(req) {
         throw Error("Could not find CSRF token in request body");
     }
 
-    if (providedCsrf !== req.session['csrf']) {
+    if (providedCsrf !== session['csrf']) {
         throw Error("Could not verify CSRF token.");
     }
     
